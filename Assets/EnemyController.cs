6522446584be61979d888b6f3667d5b98467006a
Jmax2244 <olikin1999@gmail.com>
2025-04-30 09:40:49 +0200

    zrobione zadanie z e-tebu hp przeciwinkow

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -2,22 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+
 public class EnemyController : MonoBehaviour
 {
     GameObject player;
     public float speed = 4f;
     LevelManager lm;
     // Start is called before the first frame update
+
+    public int maxHP = 10;
+    private int currentHP;
+
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         lm = GameObject.Find("LevelManager").GetComponent<LevelManager>();
+        currentHP = maxHP;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHP -= amount;
+        if (currentHP <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        lm.AddPoints(1);
+        Destroy(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     private void FixedUpdate()
     {
@@ -26,11 +49,15 @@ public class EnemyController : MonoBehaviour
     }
     public void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("PlayerWeapon"))
+        if (other.gameObject.CompareTag("PlayerWeapon"))
         {
-            lm.AddPoints(1);
-            Destroy(gameObject);
-            //Destroy(other.gameObject);
+            currentHP -= 5; // ? sta³a wartoœæ obra¿eñ, mo¿esz tu zmieniæ na 1, 10 itd.
+
+            if (currentHP <= 0)
+            {
+                lm.AddPoints(1);
+                Destroy(gameObject);
+            }
         }
     }
 }
